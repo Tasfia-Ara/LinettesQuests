bbcdb25235023f816ba9a9d60e2033f823a1d051
Tasfia-Ara <62263716+Tasfia-Ara@users.noreply.github.com>
2026-02-23 21:23:50 -0500

    Add files via upload

---
+++ LinettesQuestsTests/LinettesQuestsTests.swift
@@ -0,0 +1,17 @@
+//
+//  LinettesQuestsTests.swift
+//  LinettesQuestsTests
+//
+//  Created by Tasfia Ara on 2026-02-20.
+//
+
+import Testing
+@testable import LinettesQuests
+
+struct LinettesQuestsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
